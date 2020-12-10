97f989e29735ebbfe2b5fcc18cbc7754aad9d448
Timnik <72356939+Timnikk@users.noreply.github.com>
2020-12-10 21:05:20 +0200

    Create SimpleCalculator.py

---
+++ Beginner/SimpleCalculator.py
@@ -0,0 +1,28 @@
+## Simple calculator 
+
+## Choose what operation you want to do
+print("Simple Calculator, created by Timnik")
+print("Select what operation you want to do: ")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+print(" ")
+x = int(input("Enter: "))
+print(" ")
+
+## Enter the 2 numbers
+print("Select the two numbers:")
+print(" ")
+z = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+if (x==1):
+    print(str(z) + " + " + str(y) + " = " + str(z + y))
+elif (x==2):
+    print(str(z) + " - " + str(y) + " = " + str(z - y))
+elif (x==3):
+    print(str(z) + " * " + str(y) + " = " + str(z * y))
+elif (x==4):
+    print(str(z) + " / " + str(y) + " = " + str(z / y))
+else:
+    print("You enter an invalid argument. The only valid arguments are: 1, 2, 3 or 4")
